cfc90ba136f8a8544051a7a6d0f89d443d788bc8
t0pep0 <t0pep0.gentoo@gmail.com>
2014-03-05 16:59:48 +0400

    fix require

--- lib/pushka.rb
+++ lib/pushka.rb
@@ -1,4 +1,10 @@
 require "pushka/version"
+require "configuration/configuration"
+require "generators/install_generator"
+require "pushka/devices"
+require "pushka/errors"
+require "pushka/push"
+require "pushka/queue"
 
 module Pushka
   # Your code goes here...
